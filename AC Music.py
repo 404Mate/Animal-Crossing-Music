4b67f9e35f443635c4ced6889ab8d9eb0a4470dc
404Mate <cadenstolz05@Gmail.com>
2022-04-26 14:53:39 -0500

    YOLO Update

--- AC Music.py
+++ AC Music.py
@@ -1,106 +1,197 @@
 # get dependancies
-import asyncio
-from asyncore import loop
 import time
 from playsound import playsound
+import os 
 
 # Get Current Time, save current hour to var
 Time = time.localtime()
 current_time = time.strftime("%H", Time)
 hour = time.strftime("%H", Time)
 
-#play newleaf music without weather
+
+
+# determine weather, pass it off to the weather specific functions
+print("TESTING What do you want the weather to be?")
+weather = input("""1: Sunny
+2: Rainy
+3: Snowy
+""")
+if weather == "1":
+    def sunnynewleaf():
+    # Get Current Time, save current hour to var and move to the songs folder
+        Time = time.localtime()
+        current_time = time.strftime("%H", Time)
+        hour = time.strftime("%H", Time)
+        if hour == "00":
+                playsound("12AM.mp3")
+                sunnynewleaf()()
+        elif hour == "01":
+                playsound("1AM.mp3")
+                sunnynewleaf()()
+        elif hour == "02":
+                playsound("2AM.mp3")
+                sunnynewleaf()()
+        elif hour == "03":
+                playsound("3AM.mp3")
+                sunnynewleaf()()
+        elif hour == "04":
+                playsound("4AM.mp3")
+                sunnynewleaf()()
+        elif hour == "05":
+                playsound("5AM.mp3")
+                sunnynewleaf()()
+        elif hour == "06":
+                playsound("6AM.mp3")
+                sunnynewleaf()()
+        elif hour == "07":
+                playsound("7AM.mp3")
+                sunnynewleaf()()
+        elif hour == "08":
+                playsound("8AM.mp3")
+                sunnynewleaf()()
+        elif hour == "09":
+                playsound("9AM.mp3")
+                sunnynewleaf()()
+        elif hour == "10":
+                playsound("10AM.mp3")
+                sunnynewleaf()()
+        elif hour == "11":
+                playsound("11AM.mp3")
+                sunnynewleaf()()
+        elif hour == "12":
+                playsound("12PM.mp3")
+                sunnynewleaf()()
+        elif hour == "13":
+                playsound("1PM.mp3")
+                sunnynewleaf()()
+        elif hour == "14":
+                playsound("2PM.mp3")
+                sunnynewleaf()()
+        elif hour == "15":
+                playsound("3PM.mp3")
+                sunnynewleaf()()
+        elif hour == "16":
+                playsound("4PM.mp3")
+                sunnynewleaf()()
+        elif hour == "17":
+                playsound("5PM.mp3")
+                sunnynewleaf()()
+        elif hour == "18":
+                playsound("6PM.mp3")
+                sunnynewleaf()()
+        elif hour == "19":
+                playsound("7PM.mp3")
+                sunnynewleaf()()
+        elif hour == "20":
+                playsound("8PM.mp3")
+                sunnynewleaf()()
+        elif hour == "21":
+                playsound("9PM.mp3")
+                sunnynewleaf()()
+        elif hour == "22":
+                playsound("10PM.mp3")
+                sunnynewleaf()()
+        elif hour == "23":
+                playsound("11PM.mp3")
+                sunnynewleaf()()
+        else:
+                print("Something is wrong with detecting the time, please try again later or make a bug report")
+                exit()
+
+# play newleaf music without weather
 def noweathernewleafhourly():
-    # Get Current Time, save current hour to var
+    # Get Current Time, save current hour to var and move to the songs folder
     Time = time.localtime()
     current_time = time.strftime("%H", Time)
     hour = time.strftime("%H", Time)
     if hour == "00":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/12AM.mp3")
-        noweathernewleafhourly()
+            playsound("12AM.mp3")
+            noweathernewleafhourly()
     elif hour == "01":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/1AM.mp3")
-        noweathernewleafhourly()
+            playsound("1AM.mp3")
+            noweathernewleafhourly()
     elif hour == "02":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/2AM.mp3")
-        noweathernewleafhourly()
+            playsound("2AM.mp3")
+            noweathernewleafhourly()
     elif hour == "03":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/3AM.mp3")
-        noweathernewleafhourly()
+            playsound("3AM.mp3")
+            noweathernewleafhourly()
     elif hour == "04":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/4AM.mp3")
-        noweathernewleafhourly()
+            playsound("4AM.mp3")
+            noweathernewleafhourly()
     elif hour == "05":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/5AM.mp3")
-        noweathernewleafhourly()
+            playsound("5AM.mp3")
+            noweathernewleafhourly()
     elif hour == "06":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/6AM.mp3")
-        noweathernewleafhourly()
+            playsound("6AM.mp3")
+            noweathernewleafhourly()
     elif hour == "07":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/7AM.mp3")
-        noweathernewleafhourly()
+            playsound("7AM.mp3")
+            noweathernewleafhourly()
     elif hour == "08":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/8AM.mp3")
-        noweathernewleafhourly()
+            playsound("8AM.mp3")
+            noweathernewleafhourly()
     elif hour == "09":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/9AM.mp3")
-        noweathernewleafhourly()
+            playsound("9AM.mp3")
+            noweathernewleafhourly()
     elif hour == "10":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/10AM.mp3")
-        noweathernewleafhourly()
+            playsound("10AM.mp3")
+            noweathernewleafhourly()
     elif hour == "11":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/11AM.mp3")
-        noweathernewleafhourly()
+            playsound("11AM.mp3")
+            noweathernewleafhourly()
     elif hour == "12":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/12PM.mp3")
-        noweathernewleafhourly()
+            playsound("12PM.mp3")
+            noweathernewleafhourly()
     elif hour == "13":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/1PM.mp3")
-        noweathernewleafhourly()
+            playsound("1PM.mp3")
+            noweathernewleafhourly()
     elif hour == "14":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/2PM.mp3")
-        print("secondloop,")
-        noweathernewleafhourly()
+            playsound("2PM.mp3")
+            noweathernewleafhourly()
     elif hour == "15":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/3PM.mp3")
-        noweathernewleafhourly()
+            playsound("3PM.mp3")
+            noweathernewleafhourly()
     elif hour == "16":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/4PM.mp3")
-        noweathernewleafhourly()
+            playsound("4PM.mp3")
+            noweathernewleafhourly()
     elif hour == "17":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/5PM.mp3")
-        noweathernewleafhourly()
+            playsound("5PM.mp3")
+            noweathernewleafhourly()
     elif hour == "18":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/6PM.mp3")
-        noweathernewleafhourly()
+            playsound("6PM.mp3")
+            noweathernewleafhourly()
     elif hour == "19":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/7PM.mp3")
-        noweathernewleafhourly()
+            playsound("7PM.mp3")
+            noweathernewleafhourly()
     elif hour == "20":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/8PM.mp3")
-        noweathernewleafhourly()
+            playsound("8PM.mp3")
+            noweathernewleafhourly()
     elif hour == "21":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/9PM.mp3")
-        noweathernewleafhourly()
+            playsound("9PM.mp3")
+            noweathernewleafhourly()
     elif hour == "22":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/10PM.mp3")
-        noweathernewleafhourly()
+            playsound("10PM.mp3")
+            noweathernewleafhourly()
     elif hour == "23":
-        playsound("AC Music/Animal-Crossing-Music/hourly songs/11PM.mp3")
-        noweathernewleafhourly()
+            playsound("11PM.mp3")
+            noweathernewleafhourly()
     else:
-        print("Something is wrong with detecting the time, please try again later or make a bug report")
-        exit()
+            print("Something is wrong with detecting the time, please try again later or make a bug report")
+            exit()
    
-#sanity check, current hour in 24h format
+# sanity check, current hour in 24h format
 print("The Hour is", hour)
 
-#collect zipcode/skip zipcode
+# collect zipcode/skip zipcode
 print("Please enter your zipcode for weather or skip to ignore")
 zipcode = input("Zipcode:")
 if zipcode == "skip":
     print("Ignoring weather,")
+    os.chdir("songs")
     noweathernewleafhourly()
 else:
+    os.chdir("songs")
     noweathernewleafhourly()
-#zip code not collected for now, no use
+# zip code not collected for now, no use
+
---
+++ songs/raintest.py
@@ -0,0 +1,10 @@
+import os
+import playsound
+
+print(os.getcwd())
+os.chdir("songs")
+print(os.getcwd())
+os.chdir("rain")
+print(os.getcwd())
+os.chdir("..")
+print(os.getcwd())
