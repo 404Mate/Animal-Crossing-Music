0f1f92d49fcdbb19751847edbfd03fb46360bc87
404Mate <cadenstolz05@Gmail.com>
2022-05-16 13:43:38 -0500

    Update song formats, work on player, weather test

--- AC Music.py
+++ AC Music.py
@@ -8,97 +8,19 @@ Time = time.localtime()
 current_time = time.strftime("%H", Time)
 hour = time.strftime("%H", Time)
 
-
-
-# determine weather, pass it off to the weather specific functions
-print("TESTING What do you want the weather to be?")
-weather = input("""1: Sunny
-2: Rainy
-3: Snowy
-""")
-if weather == "1":
-    def sunnynewleaf():
-    # Get Current Time, save current hour to var and move to the songs folder
-        Time = time.localtime()
-        current_time = time.strftime("%H", Time)
-        hour = time.strftime("%H", Time)
-        if hour == "00":
-                playsound("12AM.mp3")
-                sunnynewleaf()()
-        elif hour == "01":
-                playsound("1AM.mp3")
-                sunnynewleaf()()
-        elif hour == "02":
-                playsound("2AM.mp3")
-                sunnynewleaf()()
-        elif hour == "03":
-                playsound("3AM.mp3")
-                sunnynewleaf()()
-        elif hour == "04":
-                playsound("4AM.mp3")
-                sunnynewleaf()()
-        elif hour == "05":
-                playsound("5AM.mp3")
-                sunnynewleaf()()
-        elif hour == "06":
-                playsound("6AM.mp3")
-                sunnynewleaf()()
-        elif hour == "07":
-                playsound("7AM.mp3")
-                sunnynewleaf()()
-        elif hour == "08":
-                playsound("8AM.mp3")
-                sunnynewleaf()()
-        elif hour == "09":
-                playsound("9AM.mp3")
-                sunnynewleaf()()
-        elif hour == "10":
-                playsound("10AM.mp3")
-                sunnynewleaf()()
-        elif hour == "11":
-                playsound("11AM.mp3")
-                sunnynewleaf()()
-        elif hour == "12":
-                playsound("12PM.mp3")
-                sunnynewleaf()()
-        elif hour == "13":
-                playsound("1PM.mp3")
-                sunnynewleaf()()
-        elif hour == "14":
-                playsound("2PM.mp3")
-                sunnynewleaf()()
-        elif hour == "15":
-                playsound("3PM.mp3")
-                sunnynewleaf()()
-        elif hour == "16":
-                playsound("4PM.mp3")
-                sunnynewleaf()()
-        elif hour == "17":
-                playsound("5PM.mp3")
-                sunnynewleaf()()
-        elif hour == "18":
-                playsound("6PM.mp3")
-                sunnynewleaf()()
-        elif hour == "19":
-                playsound("7PM.mp3")
-                sunnynewleaf()()
-        elif hour == "20":
-                playsound("8PM.mp3")
-                sunnynewleaf()()
-        elif hour == "21":
-                playsound("9PM.mp3")
-                sunnynewleaf()()
-        elif hour == "22":
-                playsound("10PM.mp3")
-                sunnynewleaf()()
-        elif hour == "23":
-                playsound("11PM.mp3")
-                sunnynewleaf()()
-        else:
-                print("Something is wrong with detecting the time, please try again later or make a bug report")
-                exit()
-
-# play newleaf music without weather
+# functions for different weathers/no weather
+def updateweather():
+    if weather == "1":
+        os.chdir("songs")
+        sunnynewleaf()
+    elif weather == "2":
+        os.chdir("songs")
+        os.chdir("rain")
+        rainynewleaf()
+    elif weather == "3":
+        os.chdir("songs")
+        os.chdir("snow")
+        snowynewleaf()
 def noweathernewleafhourly():
     # Get Current Time, save current hour to var and move to the songs folder
     Time = time.localtime()
@@ -179,19 +101,207 @@ def noweathernewleafhourly():
     else:
             print("Something is wrong with detecting the time, please try again later or make a bug report")
             exit()
-   
+def sunnynewleaf():
+    # Get Current Time, save current hour to var and move to the songs folder
+    Time = time.localtime()
+    current_time = time.strftime("%H", Time)
+    hour = time.strftime("%H", Time)
+    if hour == "00":
+        playsound("12AM.mp3")
+        sunnynewleaf()()
+    elif hour == "01":
+            playsound("1AM.mp3")
+            sunnynewleaf()()
+    elif hour == "02":
+        playsound("2AM.mp3")
+        sunnynewleaf()()
+    elif hour == "03":
+        playsound("3AM.mp3")
+        sunnynewleaf()()
+    elif hour == "04":
+        playsound("4AM.mp3")
+        sunnynewleaf()()
+    elif hour == "05":
+        playsound("5AM.mp3")
+        sunnynewleaf()()
+    elif hour == "06":
+        playsound("6AM.mp3")
+        sunnynewleaf()()
+    elif hour == "07":
+        playsound("7AM.mp3")
+        sunnynewleaf()()
+    elif hour == "08":
+        playsound("8AM.mp3")
+        sunnynewleaf()()
+    elif hour == "09":
+        playsound("9AM.mp3")
+        sunnynewleaf()()
+    elif hour == "10":
+        playsound("10AM.mp3")
+        sunnynewleaf()()
+    elif hour == "11":
+        playsound("11AM.mp3")
+        sunnynewleaf()()
+    elif hour == "12":
+        playsound("12PM.mp3")
+        sunnynewleaf()()
+    elif hour == "13":
+        playsound("1PM.mp3")
+        sunnynewleaf()()
+    elif hour == "14":
+        playsound("2PM.mp3")
+        sunnynewleaf()()
+    elif hour == "15":
+        playsound("3PM.mp3")
+        sunnynewleaf()()
+    elif hour == "16":
+        playsound("4PM.mp3")
+        sunnynewleaf()()
+    elif hour == "17":
+        playsound("5PM.mp3")
+        sunnynewleaf()()
+    elif hour == "18":
+        playsound("6PM.mp3")
+        sunnynewleaf()()
+    elif hour == "19":
+        playsound("7PM.mp3")
+        sunnynewleaf()()
+    elif hour == "20":
+        playsound("8PM.mp3")
+        sunnynewleaf()()
+    elif hour == "21":
+        playsound("9PM.mp3")
+        sunnynewleaf()()
+    elif hour == "22":
+        playsound("10PM.mp3")
+        sunnynewleaf()()
+    elif hour == "23":
+        playsound("11PM.mp3")
+        sunnynewleaf()()
+    else:
+        print("Something is wrong with detecting the time, please try again later or make a bug report")
+        exit()
+def rainynewleaf():
+    # Get Current Time, save current hour to var and move to the songs folder
+
+    Time = time.localtime()
+    current_time = time.strftime("%H", Time)
+    hour = time.strftime("%H", Time)
+    if hour == "00":
+        playsound("12AMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "01":
+            playsound("1AMRainy.mp3")
+            rainynewleaf()()
+    elif hour == "02":
+        playsound("2AMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "03":
+        playsound("3AMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "04":
+        playsound("4AMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "05":
+        playsound("5AMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "06":
+        playsound("6AMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "07":
+        playsound("7AMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "08":
+        playsound("8AMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "09":
+        playsound("9AMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "10":
+        playsound("10AMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "11":
+        playsound("11AMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "12":
+        playsound("12PMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "13":
+        playsound("1PMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "14":
+        playsound("2PMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "15":
+        playsound("3PMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "16":
+        playsound("4PMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "17":
+        playsound("5PMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "18":
+        playsound("6PMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "19":
+        playsound("7PMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "20":
+        playsound("8PMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "21":
+        playsound("9PMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "22":
+        playsound("10PMRainy.mp3")
+        rainynewleaf()()
+    elif hour == "23":
+        playsound("11PMRainy.mp3")
+        rainynewleaf()()
+    else:
+        print("Something is wrong with detecting the time, please try again later or make a bug report")
+        exit()
+def snowynewleaf():
+    print("no snow yet, come back later")
+    exit()
+
 # sanity check, current hour in 24h format
+
 print("The Hour is", hour)
 
 # collect zipcode/skip zipcode
-print("Please enter your zipcode for weather or skip to ignore")
-zipcode = input("Zipcode:")
-if zipcode == "skip":
-    print("Ignoring weather,")
-    os.chdir("songs")
-    noweathernewleafhourly()
-else:
-    os.chdir("songs")
-    noweathernewleafhourly()
-# zip code not collected for now, no use
+os.chdir("songs")
+print("""How do you want weather?
+1: Zipcode
+2: Manual
+3 Skip
+4: Exit""")
 
+weatherchoice = input()
+if weatherchoice == "1":
+    print("This is not yet available")
+    exit()
+elif weatherchoice == "2":
+    print("""
+    1: Sunny
+    2: Rainy
+    3: Snowy""")
+    weather = input()
+    if weather == "1":
+        sunnynewleaf()
+    elif weather == "2":
+        
+        rainynewleaf()
+    elif weather == "3":
+        snowynewleaf()
+    else:
+        print("Sorry, that was not recognized")
+        exit()
+elif weatherchoice == "3":
+    noweathernewleafhourly()
+elif weatherchoice == "4":
+    print("Thank you, goodbye")
+else:
+    print("Sorry, that was not recognized")
+    exit()
