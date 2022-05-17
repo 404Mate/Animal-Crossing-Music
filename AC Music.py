625d28cb22b98edc8a7ce4f8c67e40b34e37fae1
404Mate <cadenstolz05@Gmail.com>
2022-05-17 14:51:23 -0500

    remove ~90 lines by reusing sunnynewleaf

--- AC Music.py
+++ AC Music.py
@@ -50,86 +50,7 @@ def updateweather():
         sunnynewleaf()
     else:
         print("Something went wrong and im too lazy to figure it out")
-def noweathernewleafhourly():
-    # Get Current Time, save current hour to var and move to the songs folder
-    Time = time.localtime()
-    current_time = time.strftime("%H", Time)
-    hour = time.strftime("%H", Time)
-    if hour == "00":
-            playsound("12AM.mp3")
-            noweathernewleafhourly()
-    elif hour == "01":
-            playsound("1AM.mp3")
-            noweathernewleafhourly()
-    elif hour == "02":
-            playsound("2AM.mp3")
-            noweathernewleafhourly()
-    elif hour == "03":
-            playsound("3AM.mp3")
-            noweathernewleafhourly()
-    elif hour == "04":
-            playsound("4AM.mp3")
-            noweathernewleafhourly()
-    elif hour == "05":
-            playsound("5AM.mp3")
-            noweathernewleafhourly()
-    elif hour == "06":
-            playsound("6AM.mp3")
-            noweathernewleafhourly()
-    elif hour == "07":
-            playsound("7AM.mp3")
-            noweathernewleafhourly()
-    elif hour == "08":
-            playsound("8AM.mp3")
-            noweathernewleafhourly()
-    elif hour == "09":
-            playsound("9AM.mp3")
-            noweathernewleafhourly()
-    elif hour == "10":
-            playsound("10AM.mp3")
-            noweathernewleafhourly()
-    elif hour == "11":
-            playsound("11AM.mp3")
-            noweathernewleafhourly()
-    elif hour == "12":
-            playsound("12PM.mp3")
-            noweathernewleafhourly()
-    elif hour == "13":
-            playsound("1PM.mp3")
-            noweathernewleafhourly()
-    elif hour == "14":
-            playsound("2PM.mp3")
-            noweathernewleafhourly()
-    elif hour == "15":
-            playsound("3PM.mp3")
-            noweathernewleafhourly()
-    elif hour == "16":
-            playsound("4PM.mp3")
-            noweathernewleafhourly()
-    elif hour == "17":
-            playsound("5PM.mp3")
-            noweathernewleafhourly()
-    elif hour == "18":
-            playsound("6PM.mp3")
-            noweathernewleafhourly()
-    elif hour == "19":
-            playsound("7PM.mp3")
-            noweathernewleafhourly()
-    elif hour == "20":
-            playsound("8PM.mp3")
-            noweathernewleafhourly()
-    elif hour == "21":
-            playsound("9PM.mp3")
-            noweathernewleafhourly()
-    elif hour == "22":
-            playsound("10PM.mp3")
-            noweathernewleafhourly()
-    elif hour == "23":
-            playsound("11PM.mp3")
-            noweathernewleafhourly()
-    else:
-            print("Something is wrong with detecting the time, please try again later or make a bug report")
-            exit()
+
 def sunnynewleaf():
     # Get Current Time, save current hour to var and move to the songs folder
     Time = time.localtime()
@@ -645,7 +566,7 @@ elif weatherchoice == "2":
         print("Sorry, that was not recognized")
         exit()
 elif weatherchoice == "3":
-    noweathernewleafhourly()
+    sunnynewleaf()
 elif weatherchoice == "4":
     print("Thank you, goodbye")
 else:
